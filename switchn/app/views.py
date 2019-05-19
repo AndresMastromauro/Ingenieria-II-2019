9463b174cf99477d1b229f26f520bd945bd1ab57
Schelotto, Jorge Alberto <jasfotografo@hotmail.com>
2019-05-19 01:19:39 -0300

    Agregada vista about, form pujar(Aun no carga nada en la BD)

--- switchn/app/views.py
+++ switchn/app/views.py
@@ -2,15 +2,21 @@ from django.shortcuts import render
 from .models import *
 
 
+
 def home(request):
     context = {
-        'posts': PropiedadLiviana.objects.all()
+        'posts': PropiedadLiviana.objects.all().order_by('pk').reverse()
     }
     return render(request, 'app/propiedades_base.html', context)
 
 
 def about(request):
-    return render(request, 'app/about.html', {'title': 'About'})
+
+    contextSubastas = {
+
+        'subastas':  Subasta.objects.all().order_by('precioBase').distinct(),
+    }
+    return render(request, 'app/about.html', contextSubastas, {'title': 'About'})
 
 
 def detail_auction(request, pk):
