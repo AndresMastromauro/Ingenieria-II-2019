2fd934f319f82804b2a91f3dd4849d543151b068
Ignacio Hurtado <ignaciohurtado91@gmail.com>
2019-06-01 14:15:55 -0300

    config y reqs para heroku-postgresql

--- switchn/switchn/settings.py
+++ switchn/switchn/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-import os
+import os, subprocess
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,11 +75,17 @@ WSGI_APPLICATION = 'switchn.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+import dj_database_url
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=subprocess.run(
+            "heroku config:get DATABASE_URL --app switchn",
+            shell=True,
+            stdout=subprocess.PIPE
+        ).stdout,
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
